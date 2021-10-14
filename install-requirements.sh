5b0f4ba66cfd8378461594de2527a006b3dddde6
CroDoc <milicevic.andrija@gmail.com>
2021-10-14 15:38:44 +0200

    fixed install script

--- install-requirements.sh
+++ install-requirements.sh
@@ -1,9 +1,15 @@
+apt-get update
+apt-get upgrade -y
+
+pip install --upgrade pip setuptools wheel
 pip install notebook
 
-pip install https://github.com/CellProfiling/HPA-Cell-Segmentation/archive/master.zip
 apt-get install git -y
+
+pip install https://github.com/CellProfiling/HPA-Cell-Segmentation/archive/master.zip
 pip install 'git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI'
 pip install pandas
+
 apt-get install libgl1-mesa-glx -y
 apt-get install libglib2.0-0 -y
 
@@ -11,7 +17,6 @@ pip install ipywidgets
 jupyter nbextension enable --py widgetsnbextension
 
 pip install opencv-python
-pip install pylibjpeg pylibjpeg-libjpeg pydicom
 
 pip install efficientnet_pytorch
 pip install grad-cam
