db6ba6244e9170c27e98729d72bb10ed8864a97e
CroDoc <milicevic.andrija@gmail.com>
2021-10-19 16:46:47 +0200

    move CAM_results to results

--- scripts/gradcam_test_short.sh
+++ scripts/gradcam_test_short.sh
@@ -1,4 +1,4 @@
-python run_test.py -i CAM_images -c CAM_cells -n CAM_nuclei -d CAM_dataset -f CAM_results -o results_short -s 2 -m b0 --no_postprocessing
-python run_test.py -i CAM_images -c CAM_cells -n CAM_nuclei -d CAM_dataset -f CAM_results -o results_short -a -s 2 -m b0
-python run_test.py -i CAM_images -c CAM_cells -n CAM_nuclei -d CAM_dataset -f CAM_results -o results_short -a -s 2 -m final_ensemble_1
-python run_test.py -i CAM_images -c CAM_cells -n CAM_nuclei -d CAM_dataset -f CAM_results -o results_short -a -s 2 -m final_ensemble_2
+python run_test.py -i CAM_images -c CAM_cells -n CAM_nuclei -d CAM_dataset -f results -o results_short -s 2 -m b0 --no_postprocessing
+python run_test.py -i CAM_images -c CAM_cells -n CAM_nuclei -d CAM_dataset -f results -o results_short -a -s 2 -m b0
+python run_test.py -i CAM_images -c CAM_cells -n CAM_nuclei -d CAM_dataset -f results -o results_short -a -s 2 -m final_ensemble_1
+python run_test.py -i CAM_images -c CAM_cells -n CAM_nuclei -d CAM_dataset -f results -o results_short -a -s 2 -m final_ensemble_2
